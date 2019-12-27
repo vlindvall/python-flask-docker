e1389d09a8dbb95bbf46fdbcbca8d8c5d650e2c3
vlindvall <vlindvall@xometry.com>
2019-12-27 14:48:34 -0500

    add test

---
+++ app/tests.py
@@ -0,0 +1,14 @@
+import pytest
+
+@pytest.mark.parameterize(
+'''note, is_equal, expected_output
+''',
+    [
+       ('correct', 1, True),
+       ('incorrect', 2, False),
+    ]
+)
+def test_pytest(note, is_equal, expected_output):
+    number = 1
+    equal = (is_equal == number)
+    assert equal == expected_output
