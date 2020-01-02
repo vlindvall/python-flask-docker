094ab4e9b09879c3d5df80a0d84c95c2533fd8a1
vlindvall <vlindvall@xometry.com>
2020-01-02 09:42:30 -0500

    add more tests

--- app/tests.py
+++ app/tests.py
@@ -12,3 +12,31 @@ def test_pytest(note, is_equal, expected_output):
     number = 1
     equal = (is_equal == number)
     assert equal == expected_output
+
+
+@pytest.mark.parametrize(
+'''note, is_equal, expected_output
+''',
+    [
+       (1, 1, True),
+       (1, 2, False),
+    ]
+)
+def test_pytest2(note, is_equal, expected_output):
+    equal = (is_equal == note)
+    assert equal == expected_output
+
+
+
+@pytest.mark.parametrize(
+'''note, is_equal, expected_output
+''',
+    [
+       (2, 2, True),
+       (2, 2, True),
+    ]
+)
+def test_pytest2(note, is_equal, expected_output):
+    equal = (is_equal == note)
+    assert equal == expected_output
+
